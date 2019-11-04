e460979c4cf503d280e839b5f284e1d04623c96a
WeiYuanStudio <WeiYuanStudio@gmail.com>
2019-11-04 23:23:49 +0800

    :art: 增加测试性功能时间戳，加入请求返回结果打印

--- DoCourse.py
+++ DoCourse.py
@@ -9,7 +9,7 @@ COURSE_ID = 1012  # 课程ID
 
 if __name__ == '__main__':
     now = datetime.now()
-    time_stamp = int(datetime.timestamp(now) * 100) - 3000  # 时间戳从系统获取
+    time_stamp = int(datetime.timestamp(now) * 1000) - 3000  # 时间戳从系统获取
     f = open('.\\answer.json', 'r')  # 打开文件
     answer_json_text = f.read()  # 读文件
     f.close()  # 关闭文件
@@ -25,6 +25,7 @@ if __name__ == '__main__':
         req_ctx = WXUtil.gen_req_ctx(em, COURSE_ID, time_stamp + random.randint(1, 10) * 1000,
                                      '2_' + str(em['em_id']))  # 生成请求参数data，且加入时间波动
         print(req_ctx)  # 显示参数内容
-        WXAPI.doCourse(req_ctx, time_stamp, COURSE_ID, '2_' + str(em['em_id']), cookie)  # 发送请求
+        rsp_ctx = WXAPI.doCourse(req_ctx, time_stamp, COURSE_ID, '2_' + str(em['em_id']), cookie)  # 发送请求
+        print(rsp_ctx)
         time.sleep(0.5)  # 休息0.5秒
     print('Finished!')
