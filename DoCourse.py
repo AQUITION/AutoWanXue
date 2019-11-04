a732327c69a86858cc6adaba257049f59273dc13
WeiYuanStudio <WeiYuanStudio@gmail.com>
2019-11-04 22:44:14 +0800

    :sparkles: 自动从系统获取timestamp

--- DoCourse.py
+++ DoCourse.py
@@ -1,13 +1,15 @@
 import json
 import random
 import time
+from datetime import datetime
 import WanXue.WXAPI as WXAPI
 import WanXue.WXUtil as WXUtil
 
 COURSE_ID = 1012  # 课程ID
-time_stamp = None  # TODO：时间戳问题待解决，这里请自行替换
 
 if __name__ == '__main__':
+    now = datetime.now()
+    time_stamp = int(datetime.timestamp(now) * 100) - 3000  # 时间戳从系统获取
     f = open('.\\answer.json', 'r')  # 打开文件
     answer_json_text = f.read()  # 读文件
     f.close()  # 关闭文件
@@ -21,7 +23,7 @@ if __name__ == '__main__':
     for em in answer_json:
         print(em)
         req_ctx = WXUtil.gen_req_ctx(em, COURSE_ID, time_stamp + random.randint(1, 10) * 1000,
-                                     '2_' + str(em['em_id']))  # 生成请求参数data
+                                     '2_' + str(em['em_id']))  # 生成请求参数data，且加入时间波动
         print(req_ctx)  # 显示参数内容
         WXAPI.doCourse(req_ctx, time_stamp, COURSE_ID, '2_' + str(em['em_id']), cookie)  # 发送请求
         time.sleep(0.5)  # 休息0.5秒
